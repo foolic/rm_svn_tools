c7fe6153a47a94d6bf014d2fc05c3e8bebd7be67
ssc <ssc.santamonica@gmail.com>
2015-01-20 23:35:05 +0900

    Update post-commit.bat

--- post-commit.bat
+++ post-commit.bat
@@ -1,5 +1,2 @@
-@echo off
-set REPOS=%1
-set REV=%2
-
-$Roby Install Path$/ruby -ropen-uri -e 'open("http://Redmine_servername/sys/fetch_changesets?key=APIKEY")'
+echo off
+cscript "%~dp0refresh_redmine.vbs" //Nologo >> "%~dp0refresh_redmine.log" 2>&1
